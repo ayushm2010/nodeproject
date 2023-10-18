88d4c9b7ecc1b036baad721c853b9bbf2c102209
ayushm2010 <pvt68868@gmail.com>
2023-10-18 19:32:03 +0530

    nodemailer

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -29,17 +29,17 @@ exports.insert = async (req, res) => {
         }
     });
 
-    var data = await usermodel.create(req.body);
+    // var data = await usermodel.create(req.body);
     res.status(200).json({
-        status: "success",
-        data,
+        status: "Send  Successfully ",
+        otp
     })
 }
 
 exports.getdata = async (req, res) => {
     var data = await usermodel.find()
     res.status(200).json({
-        status: "success",
+        status: "successfully otp send",
         data,
         otp
     })
