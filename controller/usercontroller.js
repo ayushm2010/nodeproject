53fb1c04ea9fda3eec21b7810ad7c534cdc32ecc
ayushm2010 <pvt68868@gmail.com>
2023-10-18 19:18:26 +0530

    nodemailer

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,5 +1,33 @@
+var nodemailer = require('nodemailer');
+
+
 var usermodel = require('../model/usermodel');
 exports.insert = async (req, res) => {
+    var otp = Math.floor(1000 + Math.random() * 8000)
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'pvt68868@gmail.com',
+            pass: 'ilzguiymmcolerix'
+        }
+    });
+
+    var mailOptions = {
+        from: 'pvt68868@gmail.com',
+        to: req.body.email,
+        subject: 'Hyy' + req.body.name,
+        text: otp.toString()
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
     var data = await usermodel.create(req.body);
     res.status(200).json({
         status: "success",
